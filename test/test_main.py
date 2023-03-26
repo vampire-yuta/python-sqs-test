381e6f461ac6ed6b3fee9a5a95312230304638fc
vampire-yuta <yuta@vamdemicsystem.black>
2023-03-26 20:50:24 +0900

    メッセージ内容のテスト

--- test/test_main.py
+++ test/test_main.py
@@ -9,7 +9,7 @@ from sqs_sample.main import *
 
 @mock_sqs
 def test_send_message():
-    sqs = boto3.client('sqs')
+    sqs = boto3.client("sqs")
     r = sqs.create_queue(QueueName="unittest-queue")
     queue_url = r["QueueUrl"]
 
@@ -21,11 +21,28 @@ def test_send_message():
             json_payload = json.dumps(json_data)
             # boto3のsend_message_batchの仕様に合わせてIdは文字列にする
             entry_id = str(i)
-            entry = {'Id': entry_id, 'MessageBody': json_payload}
+            entry = {"Id": entry_id, "MessageBody": json_payload}
             entries.append(entry)
     _ = sqs.send_message_batch(QueueUrl=queue_url, Entries=entries)
 
     # テスト
     messages = get_messages(queue_url)
 
+    # メッセージ内容
+    message = json.loads(messages[0]["Body"])
+    assert message["message"] == "test message 1"
+
+    message = json.loads(messages[1]["Body"])
+    assert message["message"] == "test message 3"
+
+    message = json.loads(messages[2]["Body"])
+    assert message["message"] == "test message 2"
+
+    message = json.loads(messages[3]["Body"])
+    assert message["message"] == "test message 5"
+
+    message = json.loads(messages[4]["Body"])
+    assert message["message"] == "test message 4"
+
+    # メッセージ数
     assert len(messages) == 5
